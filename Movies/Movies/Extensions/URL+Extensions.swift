11aa4e74809719ccf6a51f3b52bd98197e22c426
koala panda <koakoa8823@protonmail.com>
2023-12-02 22:15:21 +0900

    ファイルの追加

---
+++ Movies/Movies/Extensions/URL+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  URL+Extensions.swift
+//  Movies
+//
+//  Created by koala panda on 2023/12/02.
+//
+
+import Foundation
---
+++ Movies/Movies/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by koala panda on 2023/12/02.
+//
+
+import Foundation
